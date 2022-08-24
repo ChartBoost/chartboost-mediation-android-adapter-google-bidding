86ddc8828cb439dbf3c168a52d89517f8bd10472
chauduyphanvu <vu.chau@chartboost.com>
2022-08-24 15:31:04 -0700

    Google Bidding - Expose configurable field(s) (#6)

--- GoogleBiddingAdapter/src/main/java/com/chartboost/helium/googlebiddingadapter/GoogleBiddingAdapter.kt
+++ GoogleBiddingAdapter/src/main/java/com/chartboost/helium/googlebiddingadapter/GoogleBiddingAdapter.kt
@@ -33,6 +33,25 @@ import kotlin.coroutines.suspendCoroutine
 class GoogleBiddingAdapter : PartnerAdapter {
     companion object {
         /**
+         * List containing device IDs to be set for enabling Google Bidding test ads. It can be populated at
+         * any time and will take effect for the next ad request. Remember to empty this list or
+         * stop setting it before releasing your app.
+         */
+        public var testDeviceIds = listOf<String>()
+            set(value) {
+                field = value
+                LogController.d(
+                    "$TAG Google Bidding test device ID(s) to be set: ${
+                        if (value.isEmpty()) "none"
+                        else value.joinToString()
+                    }"
+                )
+                MobileAds.setRequestConfiguration(
+                    RequestConfiguration.Builder().setTestDeviceIds(value).build()
+                )
+            }
+
+        /**
          * The tag used for log messages.
          */
         private val TAG = "[${this::class.java.simpleName}]"
