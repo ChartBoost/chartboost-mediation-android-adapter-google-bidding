c52f1862e4a2c9b985373502a4fc57f5618ed20a
vu <vu.chau@chartboost.com>
2023-11-03 17:42:36 -0400

    [HB-6722] Google Bidding 4.22.3.0.4

--- GoogleBiddingAdapter/src/main/java/com/chartboost/mediation/googlebiddingadapter/GoogleBiddingAdapter.kt
+++ GoogleBiddingAdapter/src/main/java/com/chartboost/mediation/googlebiddingadapter/GoogleBiddingAdapter.kt
@@ -30,6 +30,7 @@ import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
 import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAdLoadCallback
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
@@ -52,9 +53,13 @@ class GoogleBiddingAdapter : PartnerAdapter {
                         else value.joinToString()
                     }"
                 )
-                MobileAds.setRequestConfiguration(
-                    RequestConfiguration.Builder().setTestDeviceIds(value).build()
-                )
+
+                // There have been known ANRs when calling setRequestConfiguration() on the main thread.
+                CoroutineScope(IO).launch {
+                    MobileAds.setRequestConfiguration(
+                        RequestConfiguration.Builder().setTestDeviceIds(value).build()
+                    )
+                }
             }
     }
 
@@ -208,16 +213,19 @@ class GoogleBiddingAdapter : PartnerAdapter {
             else COPPA_NOT_SUBJECT
         )
 
-        MobileAds.setRequestConfiguration(
-            MobileAds.getRequestConfiguration().toBuilder()
-                .setTagForChildDirectedTreatment(
-                    if (isSubjectToCoppa) {
-                        RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE
-                    } else {
-                        RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_FALSE
-                    }
-                ).build()
-        )
+        // There have been known ANRs when calling setRequestConfiguration() on the main thread.
+        CoroutineScope(IO).launch {
+            MobileAds.setRequestConfiguration(
+                MobileAds.getRequestConfiguration().toBuilder()
+                    .setTagForChildDirectedTreatment(
+                        if (isSubjectToCoppa) {
+                            RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE
+                        } else {
+                            RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_FALSE
+                        }
+                    ).build()
+            )
+        }
     }
 
     /**
